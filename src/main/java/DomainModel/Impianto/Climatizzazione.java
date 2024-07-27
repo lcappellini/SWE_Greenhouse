1762ded4cffcde65e74f46f8242920453ecbc64a
Elion <elion.karaboja@stud.unifi.it>
2024-07-27 11:31:34 +0200

    Salvando modifiche locali prima del merge con master

--- src/main/java/DomainModel/Impianto/Climatizzazione.java
+++ src/main/java/DomainModel/Impianto/Climatizzazione.java
@@ -10,17 +10,6 @@ public class Climatizzazione extends Attuatore{
         attivo = true;
     }
 
-    public String esegui(int value){
-        String descrizione = "Il climatizzatore "+this.id;
-        if(value >= 0){
-            descrizione += " è accesso.";
-            attivo = true;
-        }else{
-            descrizione += " ha terminato.";
-            attivo = false;
-        }
-        return descrizione;
-    }
 
 
     @Override
